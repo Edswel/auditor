21ac1e43ca6ef3c79b48e55cef73f0bdc9439635
Edswel <franckini@yahoo.com>
2021-03-22 22:20:37 +0100

    Style dashboard

--- src/components/Tracker/Tracker.js
+++ src/components/Tracker/Tracker.js
@@ -13,7 +13,44 @@ class Tracker extends Component {
         return (
             <div className='trackerPage'>
                 <div className='welcome'>
-                    <span>welcome, Username!</span>
+                    <span>Welcome, Username!</span>
+                    <button className='logout' onClick={this.logout}>Logout</button>
+                </div>
+                <div className='totalAmount'>
+                    $150.00
+                </div>
+                <div className='newTransactionPage'>
+                    <div className='newTransaction'>
+                        <form>
+                            <input
+                                placeholder='Expense'
+                                type='text'
+                                name='transactionName'
+                            />
+                            <div className='inputGroup'>
+                                <select name='type'>
+                                    <option value='0'>Type</option>
+                                    <option value='expense'>Expense</option>
+                                    <option value='deposit'>Deposit</option>
+                                </select>
+                                <input
+                                    placeholder='Amount'
+                                    type='text'
+                                    name='price'
+                                />
+                            </div>
+                            <button className='addTransaction'>ADD TRANSACTION</button>
+                        </form>
+                    </div>
+                </div>
+                <div className='latestTransactions'>
+                    <h5>Latest Transactions!</h5>
+                    <ul>
+                        <li>
+                            <div>Cash Transfer</div>
+                            <div>+$100</div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         );
